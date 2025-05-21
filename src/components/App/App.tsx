2121eb4342ad2ca150994c799d39a4502c56f9ea
Volodymyr <StuparenkoVladimir@gmail.com>
2025-05-21 21:39:44 +0300

    small fix App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -69,53 +69,6 @@ function App() {
     setCurrentPage(1);
   };
 
-  // const handleSearch = async (query, page) => {
-  //   setCurrentPage(page);
-  //   if (page === 1) {
-  //     setImages(() => {
-  //       return [];
-  //     });
-  //   }
-  //   try {
-  //     setLoading(true);
-  //     setError(false);
-  //     const jsonData = await searchImages(query, page);
-  //     if (
-  //       Object.keys(jsonData).length === 0 ||
-  //       !jsonData.results ||
-  //       jsonData.results.length === 0
-  //     ) {
-  //       toast.error('No results!');
-  //       setTotalPages(page);
-  //       return;
-  //     }
-  //     if (page === 1 && jsonData.total_pages) {
-  //       setTotalPages(jsonData.total_pages);
-  //     }
-  //     const newImages = jsonData.results.map((image) => {
-  //       return {
-  //         alt: image.alt_description,
-  //         blurHash: image.blur_hash,
-  //         color: image.color,
-  //         small: image.urls.small,
-  //         regular: image.urls.regular,
-  //         id: image.id,
-  //       };
-  //     });
-  //     if (page === 1) {
-  //       setImages(newImages);
-  //     } else {
-  //       setImages((prev) => {
-  //         return [...prev, ...newImages];
-  //       });
-  //     }
-  //   } catch (error) {
-  //     setError(true);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
   return (
     <div className={css.app}>
       <SearchBar handleChangeQuery={handleChangeQuery} />
